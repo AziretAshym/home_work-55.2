b3ed5bcf4319f2dd4d1e95ad9edd02fd679ef9c0
Ashymov Aziret <aziretashym@gmail.com>
2024-09-28 02:19:14 +0600

    Implemented rendering of ingredients and the burger itself

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css';
+import Ingredients from './components/Ingredients/Ingredients.tsx';
+import Burger from './components/Burger/Burger.tsx';
 
 const App = () => {
 
   return (
-    <>
-    </>
+      <>
+        <div className="container">
+          <Ingredients/>
+          <Burger/>
+        </div>
+      </>
   );
 };
 
